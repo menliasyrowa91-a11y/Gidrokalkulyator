9342399261438fa9c8849b5e4d20b4e778970313
Menli <menliasyrowa91@gmail.com>
2026-05-26 09:22:57 +0500

    Create Ekinder.js

---
+++ src/Ekinder.js
@@ -0,0 +1,7 @@
+export const EkinBazasy = {
+  gowaca: { ady: "Gowaça", norma: 2700, kpD: 0.85 },
+  bugday: { ady: "Bugdaý", norma: 1400, kpD: 0.80 },
+  arpalar: { ady: "Arpa", norma: 1200, kpD: 0.80 },
+  mekge: { ady: "Mekgejöwen", norma: 800, kpD: 0.82 },
+  // ... beýleki ekinler hem şeýle
+};
