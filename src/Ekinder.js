2d36335a60bd65ea70265a9b9da9501d27dac14f
Menli <menliasyrowa91@gmail.com>
2026-05-26 09:36:03 +0500

    Update Ekinder.js

--- src/Ekinder.js
+++ src/Ekinder.js
@@ -1,7 +1,7 @@
 export const EkinBazasy = {
-  gowaca: { ady: "Gowaça", norma: 2700, kpD: 0.85 },
-  bugday: { ady: "Bugdaý", norma: 1400, kpD: 0.80 },
-  arpalar: { ady: "Arpa", norma: 1200, kpD: 0.80 },
-  mekge: { ady: "Mekgejöwen", norma: 800, kpD: 0.82 },
-  // ... beýleki ekinler hem şeýle
+  gowaca: { ady: "Gowaça", norma: 2700, kpd: 0.85 },
+  bugday: { ady: "Bugdaý", norma: 1400, kpd: 0.80 },
+  mekge: { ady: "Mekgejöwen", norma: 800, kpd: 0.82 },
+  arpa: { ady: "Arpa", norma: 1200, kpd: 0.80 },
+  hyyar: { ady: "Hyýar", norma: 900, kpd: 0.85 },
 };
